b845c90b7ce1eda3e867aaf1bc06bb58f0605233
Mahammad <ismayilzade.mhmmdd@gmail.com>
2026-06-27 02:01:34 +0300

    add json to csv feature

--- datafile_options.py
+++ datafile_options.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pandas as pd
 
 
 DATA_FILE = "expens.json"
@@ -12,15 +13,26 @@ def get_database():
         return []
     
     else:
-        with open(DATA_FILE,"r","utf-8") as file:
+        with open(DATA_FILE,"r",encoding="utf-8") as file:
             return json.load(file)
 
 
 def write_database(expenses):
 
-    with open(DATA_FILE,"w","utf-8") as file:
-        json.dump(expenses,file)
+    with open(DATA_FILE,"w",encoding="utf-8") as file:
+        json.dump(expenses,file,indent=5)
 
+
+def json_to_csv(csv_file="expenses.csv"):
+
+    if not os.path.exists(DATA_FILE):
+        print("JSON dosyası bulunamadı.")
+        return
+
+    df = pd.read_json(DATA_FILE)
+    df.to_csv(csv_file, index=False)
+
+    print(f"{csv_file} başarıyla oluşturuldu.")
 
 
 
