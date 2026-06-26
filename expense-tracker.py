214f230fe3a3a5ca8f664b9d798d47214a0602f5
Mahammad <ismayilzade.mhmmdd@gmail.com>
2026-06-26 03:16:49 +0300

    add list and summary features

---
+++ expense-tracker.py
@@ -0,0 +1,96 @@
+import sys
+import datetime
+import datafile_options
+
+arguments = sys.argv
+
+
+class expense():
+
+    def __init__(self,id,command,description,amount):
+
+        self.command = command 
+        self.description = description
+        self.amount = amount
+        self.id = id
+
+        curr_time = datetime.datetime.now().strftime("%Y-%m-%d")
+        self.time = curr_time
+ 
+        
+
+
+    def get_dict(self):
+
+        return {
+            "ID" : self.id,
+            "Command" : self.command,
+            "Description" : self.description,
+            "Amount" : self.amount,
+            "Date" : self.time
+
+        }
+
+
+if arguments[1] == "add":
+
+    cmd = arguments[1]
+    dscpt = arguments[3]
+    amt = arguments[5]
+
+    datafile =  datafile_options.get_database()
+
+    curr_id = len(datafile) + 1
+
+    new_expense = expense(curr_id,cmd,dscpt,amt).get_dict()
+
+    datafile.append(new_expense)
+
+    datafile_options.write_database(datafile)
+
+
+elif arguments[1] == "list":
+
+    datafile =  datafile_options.get_database()
+    
+    print("#        ID           Date           Description           Amount")
+    for exp in datafile:
+        print(f"# {exp['ID']}  {exp['Date']}  {exp['Description']}  {exp['Amount']}")
+
+
+
+elif arguments[1] == "summary":
+
+    datafile =  datafile_options.get_database()
+    flag = 0
+
+    sumofexp = 0
+
+    if len(arguments) == 4:
+
+        for exp in datafile:
+            
+            date = datetime.datetime.strptime(exp["Date"],"%Y-%m-%d")
+            if date.month == int(arguments[3]):
+                sumofexp += float(exp["Amount"])
+        
+        print(f"Total expenses for {arguments[3]}th month: ${sumofexp}")
+        flag = 1
+    
+    else:
+
+        for exp in datafile:
+            sumofexp += exp["Amount"]
+        print("Total expenses: ${sumofexp}")
+        flag = 1
+
+    if flag == 0:
+        print("Control arguments")
+        sys.exit(1)
+
+
+
+
+
+        
+
